de0bd2d4b39d8947cf1d6d0749f08df4a18e65a2
darklore <zodiac.brave.story@gmail.com>
2014-06-05 02:35:45 +0900

    Add server model

---
+++ app/controllers/server/ServersController.scala
@@ -0,0 +1,56 @@
+package controllers.server
+
+import play.api.mvc.{Action, Controller}
+import play.api.data.Form
+import play.api.data.Forms._
+import models.{ Server, Servers, ServerForm }
+
+object ServersController extends Controller {
+
+  val serverForm = Form(
+    mapping(
+      "privateIpAddr" -> text
+    )(ServerForm.apply)(ServerForm.unapply)
+  )
+
+  def index = Action {
+    Ok(views.html.servers.index(Servers.all()))
+  }
+
+  def create = Action { implicit request =>
+    val form = serverForm.bindFromRequest.get
+    val server = Server(0, form.privateIpAddr)
+    Servers.insert(server)
+    Redirect(routes.ServersController.index())
+  }
+
+  def add = Action {
+    Ok(views.html.servers.add(serverForm))
+  }
+
+  def show(id: Long) = Action {
+    val server = Servers.findById(id)
+    Ok(views.html.servers.show(server))
+  }
+
+  def edit(id: Long) = Action {
+    val server = Servers.findById(id)
+    val form = ServerForm(server.privateIpAddr)
+    Ok(views.html.servers.edit(serverForm.fill(form), id))
+  }
+
+  def update(id: Long) = Action { implicit request =>
+    val form = serverForm.bindFromRequest.get
+    val server = Server(id, form.privateIpAddr)
+    Servers.update(server)
+    Redirect(routes.ServersController.show(id))
+  }
+  def update_post(id: Long) = update(id)
+
+  def delete(id: Long) = Action {
+    Servers.delete(id)
+    Redirect(routes.ServersController.index())
+  }
+  def delete_get(id: Long) = delete(id)
+
+}
---
+++ app/models/Server.scala
@@ -0,0 +1,43 @@
+package models
+
+import play.api.db.DB
+import play.api.Play.current
+import play.api.db.slick.Config.driver.simple._
+import scala.language.postfixOps
+
+case class Server(id: Long, privateIpAddr: String)
+
+object Servers {
+
+  val database = Database.forDataSource(DB.getDataSource())
+
+  class ServerTable(tag: Tag) extends Table[Server](tag, "Server") {
+    def id = column[Long]("ID", O.PrimaryKey, O.AutoInc)
+    def privateIpAddr = column[String]("PRIVATE_IP_ADDR", O.NotNull)
+    def * = (id, privateIpAddr) <>(Server.tupled, Server.unapply)
+  }
+
+  // query object
+  val servers = TableQuery[ServerTable]
+
+  def all(): List[Server] = database.withTransaction { implicit session: Session =>
+    servers.list()
+  }
+
+  def insert(s: Server) = database.withTransaction { implicit session: Session =>
+    servers.insert(s)
+  }
+
+  /** ID検索 */
+  def findById(id: Long): Server = database.withTransaction { implicit session: Session =>
+    servers.filter(_.id === id).first
+  }
+
+  def update(s: Server) = database.withTransaction { implicit session: Session =>
+    servers.filter(_.id === s.id).update(s)
+  }
+
+  def delete(id: Long) = database.withTransaction { implicit session: Session =>
+    servers.where(_.id === id).delete
+  }
+}
